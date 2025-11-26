d63a6229eebcbbeb1f8a9ded5bc4db0317b53c34
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-11-25 23:03:55 -0500

    lecture_190: creating initial classes

---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.spring.learn_spring_framework.game;
+
+public class MarioGame {
+    public void up() {
+        System.out.println("Mario Game up");
+    }
+
+    public void down() {
+        System.out.println("Mario Game down");
+    }
+
+    public void left() {
+        System.out.println("Mario Game left");
+    }
+
+    public void right() {
+        System.out.println("Mario Game right");
+    }
+}
+
